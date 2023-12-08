fc9ffae68d26f51e078f96cfd4937d0613d20644
amaanalikhan01 <amaanalikhan231@gmail.com>
2023-12-08 22:17:40 +0530

    ModulesScanner

--- analize.py
+++ analize.py
@@ -1,9 +1,12 @@
+import os
 import requests
 import base64
 import ast
+
 def get_python_script_content(repo_owner, repo_name, script_path):
     # GitHub API endpoint to get the contents of a file
-    api_url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{script_path}'   
+    api_url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{script_path}'
+
     # Make a request to the GitHub API
     response = requests.get(api_url)
 
@@ -13,7 +16,7 @@ def get_python_script_content(repo_owner, repo_name, script_path):
 
         # Check if the content is encoded in base64
         if 'content' in content_data and 'encoding' in content_data and content_data['encoding'] == 'base64':
-            content_bytes =content_data['content'].encode('utf-8')
+            content_bytes = content_data['content'].encode('utf-8')
             content_str = base64.b64decode(content_bytes).decode('utf-8')
             return content_str
 
@@ -38,17 +41,48 @@ def extract_imports_from_content(content):
 
     return imports
 
+def find_python_files(repo_owner, repo_name, folder_path=''):
+    # GitHub API endpoint to get the repository contents
+    api_url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{folder_path}'
+
+    # Make a request to the GitHub API
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        # Parse the JSON response
+        repo_contents = response.json()
+
+        # Filter for Python files
+        python_files = [file['path'] for file in repo_contents if file['name'].endswith('.py')]
+
+        # Recursively search subdirectories
+        subdirectories = [file['name'] for file in repo_contents if file['type'] == 'dir']
+        for subdir in subdirectories:
+            subdir_files = find_python_files(repo_owner, repo_name, os.path.join(folder_path, subdir))
+            if subdir_files is not None:
+                python_files += subdir_files
+
+        return python_files
+
+    else:
+        print(f"Failed to fetch repository contents. Status code: {response.status_code}")
+        return []
 if __name__ == "__main__":
-    repo_owner = 'elaa0505'
-    repo_name = 'video-edit-with-moviepy'
-    script_path = 'functions.py'
+    repo_owner = 'magic-research'
+    repo_name = 'magic-animate'
 
-    script_content = get_python_script_content(repo_owner, repo_name, script_path)
+    python_files = find_python_files(repo_owner, repo_name)
 
-    if script_content is not None:
-        # Extract imports from the script content
-        imports = extract_imports_from_content(script_content)
+    if python_files:
+        for script_path in python_files:
+            print(f"\nProcessing file: {script_path}")
+            
+            script_content = get_python_script_content(repo_owner, repo_name, script_path)
 
-        print("Imported modules:")
-        for module in sorted(imports):
-            print(module)
+            if script_content is not None:
+                # Extract imports from the script content
+                imports = extract_imports_from_content(script_content)
+
+                print("Imported modules:")
+                for module in sorted(imports):
+                    print(module)
